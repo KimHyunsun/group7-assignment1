ae2da3e07144241cd3090729820c36f4afc313dc
HSKim <hyunsun.kim0211@gmail.com>
2020-01-16 21:24:06 +0900

    first file to .py

---
+++ assignment.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+print("hello this is group 7")
+
+
+# In[ ]:
+
+
+user = int(input("How many are there in group 7?: "))
+
+
+# In[ ]:
+
+
+if user != 3:
+   print("Sorry, I don't think so")
+else:
+   print("Yay you are right! Welcome!")
+
