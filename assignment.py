327cca1551ec4aba253abaf8e45ece28c1cd8617
HSKim <hyunsun.kim0211@gmail.com>
2020-01-18 14:27:18 +0900

    added insert name line

--- assignment.py
+++ assignment.py
@@ -3,34 +3,27 @@
 
 # In[ ]:
 
-
 print("hello this is group 7 in Tobigs")
 print("Nice to meet you. Let's try our best.")
 
 # In[ ]:
 
-
 user = int(input("How many are there in group 7?: "))
 
-
 # In[ ]:
-
 
 if user != 3:
    print("Sorry, I don't think so :(")
 else:
    print("Yay you are right! Welcome! :)")
 
-
 # In[ ]:
-
 
 name=str(input("Please write your name: "))
 print('Hi, nice to meet you', name)
 
 
 # In[ ]:
-
 
 name=str(input("Please write your name: "))
 print('Hi, nice to meet you', name)
